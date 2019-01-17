d543a77e07e2370177e5f4d1f9c7d02e42d2a477
k1llxna <44785075+k1llxna@users.noreply.github.com>
2019-01-17 18:15:49 -0500

    Add files via upload

---
+++ MixerMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerMenu : MonoBehaviour {
+
+    public AudioMixer audioMix;
+
+    public void SetVolume (float v)
+    {
+        audioMix.SetFloat("volume", v);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+}
